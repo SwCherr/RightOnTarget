3eb31e25a8e7e7e1c1e472a03182249846dff4ea
Yuliya Kungurova <yuliya@macbook-pro-ulia.local>
2022-07-27 19:11:30 +0300

    Initial Commit

---
+++ Right On Target/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Right On Target
+//
+//  Created by Юлия Кунгурова on 27.07.2022.
+//  Copyright © 2022 Юлия Кунгурова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
